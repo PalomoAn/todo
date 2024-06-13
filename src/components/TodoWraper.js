1fa74b7d2a06b463078234b51b789989e539fb10
p-unit02 <72364262+PalomoAn@users.noreply.github.com>
2024-06-13 17:01:02 -0600

    Implement addTodo function to add new todo items with unique IDs, task description, and default properties

--- src/components/TodoWraper.js
+++ src/components/TodoWraper.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { TodoForm } from './TodoForm'
+
+import React, {useState} from 'react'
+import { TodoForm } from './TodoForm' 
+//ading v4
+import {v4 as uuidv4} from 'uuid';
+//initializing
+uuidv4();
 
 export const TodoWraper = () => {
+  const[todos, setTodos]=useState([])
+
+  const addTodo= todo =>{
+    setTodos([...todos,{id: uuidv4(), task: todo, completed: false, isEditing: false}])
+    console.log(todo)
+  }
   return (
     <div className='TodoWrapper'>
-        <TodoForm/>
+        <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
