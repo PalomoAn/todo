781acdb2d321f448daa3b4f85c209639c21eea78
p-unit02 <72364262+PalomoAn@users.noreply.github.com>
2024-06-13 19:20:40 -0600

    added edit todos alreasy set

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task}) => {
+  const[value,setValue]= useState("")
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    editTodo(value, task.id)
+
+
+    setValue("")
+  }
   return (
-    <div>EditTodoForm</div>
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Edit Task' />
+    <button type="submit" className='todo-btn'>Update Task</button>
+  </form>
   )
 }
--- src/components/TodoWraper.js
+++ src/components/TodoWraper.js
@@ -4,6 +4,7 @@ import { TodoForm } from './TodoForm'
 //ading v4
 import {v4 as uuidv4} from 'uuid';
 import {Todo} from './Todo';
+import { EditTodoForm } from './EditTodoForm';
 //initializing
 uuidv4();
 
@@ -25,14 +26,35 @@ export const TodoWraper = () => {
 
   const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
   
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  }
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
   return (
     <div className='TodoWrapper'>
       <h1>Notitas</h1>
         <TodoForm addTodo={addTodo}/>
         {todos.map((todo,index)=>(
+          todo.isEditing ?(
+            <EditTodoForm editTodo={editTask} task={todo}/>
+          ):(
             <Todo task ={todo} key={index} 
             toggleComplete = {toggleComplete} deleteTodo={deleteTodo}
+            editTodo={editTodo}
             />
+          )
         ))}
         
     </div>
